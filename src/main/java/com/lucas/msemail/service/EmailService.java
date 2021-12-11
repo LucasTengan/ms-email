25fc38be12608e137181d9587ebad4b9351b19f3
Lucas Tengan <lucastengan@usp.br>
2021-12-10 22:41:03 -0300

    realizando regra de negocio de envio de email

--- src/main/java/com/lucas/msemail/service/EmailService.java
+++ src/main/java/com/lucas/msemail/service/EmailService.java
@@ -1,12 +1,38 @@
 package com.lucas.msemail.service;
 
+import com.lucas.msemail.enums.StatusEmail;
+import com.lucas.msemail.model.EmailModel;
 import com.lucas.msemail.repository.EmailRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
 
     private final EmailRepository emailRepository;
+    private final JavaMailSender emailSender;
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 }
