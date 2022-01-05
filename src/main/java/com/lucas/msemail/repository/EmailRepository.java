4def6e5d06b1c03ad3f835760255e01783e3ad2d
Lucas Tengan <lucastengan@usp.br>
2022-01-05 18:48:16 -0300

    implementando comunicação assíncrona via fila com rabbitmq

--- src/main/java/com/lucas/msemail/repository/EmailRepository.java
+++ src/main/java/com/lucas/msemail/repository/EmailRepository.java
@@ -3,5 +3,7 @@ package com.lucas.msemail.repository;
 import com.lucas.msemail.model.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
